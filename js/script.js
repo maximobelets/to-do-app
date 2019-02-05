27ca06fc9224cf91736a32c4635af99bb9a654d2
Maxim Obelets <maximobelets@gmail.com>
2019-02-05 15:02:27 +0500

    Added delete function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 document.querySelector('.add-button').addEventListener('click', function() {
     var data = document.querySelector('.get-data').value;
+    var listElement = document.createElement('li');
+    var deleteButton = document.createElement('button');
 
     function addText () {
         var list = document.querySelector('.main-list');
-        var listElement = document.createElement('li');
         listElement.innerHTML = data;
         list.appendChild(listElement);
     }
+
+    function addDeleteBuutton () {
+        deleteButton.classList.add('delete-button');
+        deleteButton.innerHTML = 'Delete';
+        listElement.appendChild(deleteButton);
+    }
+
+    deleteButton.addEventListener('click', function() {
+        listElement.remove();
+    });
     
     addText();
+    addDeleteBuutton();
 });
