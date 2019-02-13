b20edc62e9d8c2f4aad9bff9e88117a77fd68fc7
Maxim Obelets <maximobelets@gmail.com>
2019-02-13 17:00:46 +0500

    Added handler for key action

--- js/script.js
+++ js/script.js
@@ -22,3 +22,30 @@ document.querySelector('.get-data__button').addEventListener('click', function()
     addText();
     addDeleteBuutton();
 });
+
+addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+        var data = document.querySelector('.get-data__input').value;
+        var listElement = document.createElement('li');
+        var deleteButton = document.createElement('button');
+
+        function addText () {
+            var list = document.querySelector('.data__list');
+            listElement.innerHTML = data;
+            listElement.classList.add('data__item');
+            list.appendChild(listElement);
+    }
+
+    function addDeleteBuutton () {
+        deleteButton.classList.add('data__button');
+        listElement.appendChild(deleteButton);
+    }
+
+    deleteButton.addEventListener('click', function() {
+        listElement.remove();
+    });
+    
+    addText();
+    addDeleteBuutton();
+    }
+});
