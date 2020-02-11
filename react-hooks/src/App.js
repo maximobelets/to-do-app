616364e7802447dcb0d7e1b4e7ef0e065aa94d44
Maxim Obelets <maximobelets@gmail.com>
2020-02-12 00:09:07 +0500

    added base logic

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const App = () => <h1>Test</h1>;
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState();
+
+  const addTodo = () => {
+    setTodos([...todos, {
+      id: todos.lenght,
+      value: value
+    }])
+  };
+
+  const addValue = (e) => {
+    setValue(e.target.value)
+  };
+
+  return(
+    <div>
+      <input onChange={addValue}/>
+      <button onClick={addTodo}>Push</button>
+      <ul>
+        {todos.map(li => {
+            return <li key={li.id}>{li.value}</li>
+        })
+        }
+      </ul>
+    </div>
+  );
+};
 
 export default App;
